63582799c31dc6073df842100de5fb6dc48f39b3
Leonardo Lima <leonardo.b.lima1@gmail.com>
2023-05-22 10:46:30 -0300

    finalizacao da pagina home

--- src/components/templates/homeTemplate/index.tsx
+++ src/components/templates/homeTemplate/index.tsx
@@ -140,6 +140,50 @@ const HomeTemplate = () => {
           </S.WrapperCardsNoticias>
           <S.ButtonVerMaisNoticias>Ver mais notícias</S.ButtonVerMaisNoticias>
         </S.Noticias>
+        <S.Footer>
+          <S.FooterLogo alt="Logo" src="assets/images/footerimg.png" />
+          <S.FooterSection1>
+            <S.FooterSection1Wrapper>
+              <img src="assets/images/globo.png" alt="globo" />
+              <div>
+                <p>Rua Codelândia, 1367</p>
+                <span>Cafelândia, SP</span>
+              </div>
+            </S.FooterSection1Wrapper>
+
+            <S.FooterSection1Wrapper>
+              <img src="assets/images/Phone.png" alt="phone" />
+              <p>+55 14 9999-9999</p>
+            </S.FooterSection1Wrapper>
+
+            <S.FooterSection1Wrapper>
+              <img src="assets/images/EnvelopeSimple.png" alt="envelope" />
+              <p>+centropaulasouza@etec.sp.gov.br</p>
+            </S.FooterSection1Wrapper>
+          </S.FooterSection1>
+
+          <S.FooterSection2>
+            <h3>Sobre a Etec Cafelândia</h3>
+            <p>
+              Com mais de 20 anos de atuação, a unidade da Etec Professora Helcy
+              Moreira Martins Aguiar se destaca no ensino médio e técnico,
+              recebendo constantemente menções e congratulações em todo o estado
+              de São Paulo.{' '}
+            </p>
+            <S.FooterSection2Wrapper>
+              <img src="assets/images/InstagramLogo.png" alt="instagram" />
+              <img src="assets/images/FacebookLogo.png" alt="facebook" />
+              <img src="assets/images/LinkedinLogo.png" alt="linkedin" />
+            </S.FooterSection2Wrapper>
+          </S.FooterSection2>
+        </S.Footer>
+
+        <S.Rodape>
+          <p>
+            Etec Professora Helcy Moreira Martins Aguiar - 2022 © Todos os
+            direitos reservados.
+          </p>
+        </S.Rodape>
       </S.Container>
     </LayoutTemplate>
   )
--- src/components/templates/homeTemplate/styles.ts
+++ src/components/templates/homeTemplate/styles.ts
@@ -14,6 +14,10 @@ export const Header = styled.header`
   top: 100px;
   background: #111827;
 
+  @media (max-width: 1420px) {
+    padding: 160px 200px;
+  }
+
   h1 {
     font-family: 'Mukta';
     font-style: normal;
@@ -172,6 +176,7 @@ export const CardCourse = styled.div`
   }
 
   @media (max-width: 420px) {
+    padding: 30px 35px;
     p {
       font-size: 24px;
     }
@@ -306,6 +311,7 @@ export const WrapperCardsNoticias = styled.div`
 
   @media (max-width: 420px) {
     flex-direction: column;
+    padding: 10px;
   }
 `
 
@@ -372,4 +378,143 @@ export const ButtonVerMaisNoticias = styled.button`
   font-size: 24px;
   line-height: 29px;
   color: #ffffff;
+`
+
+export const Footer = styled.footer`
+  padding: 70px 50px;
+  background: #b91c1c;
+  height: 350px;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  place-items: center;
+
+  @media (max-width: 420px) {
+    height: max-content;
+    padding: 50px 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+    justify-content: space-around;
+  }
+`
+
+export const FooterLogo = styled.img`
+  max-height: 80px;
+  align-self: flex-start;
+
+  @media (max-width: 420px) {
+    align-self: center;
+  }
+`
+
+export const FooterSection1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
+
+  p {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 30px;
+    color: #ffffff;
+  }
+
+  span {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+    color: #f3f4f6;
+  }
+
+  @media (max-width: 420px) {
+    justify-content: center;
+    gap: 20px;
+
+    p {
+      font-size: 14px;
+    }
+  }
+`
+
+export const FooterSection1Wrapper = styled.div`
+  display: flex;
+  gap: 24px;
+  align-items: center;
+
+  img {
+    background-color: #fff;
+    border-radius: 50%;
+    padding: 5px;
+  }
+`
+
+export const FooterSection2 = styled.div`
+  height: 100%;
+
+  h3 {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 30px;
+    color: #ffffff;
+  }
+
+  p {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 160%;
+    color: #f3f4f6;
+    max-width: 320px;
+    margin-bottom: 36px;
+  }
+
+  @media (max-width: 420px) {
+    text-align: center;
+  }
+`
+
+export const FooterSection2Wrapper = styled.div`
+  display: flex;
+  gap: 36px;
+
+  img {
+    background-color: #fff;
+    padding: 6px;
+    border-radius: 50%;
+  }
+
+  @media (max-width: 420px) {
+    justify-content: center;
+  }
+`
+
+export const Rodape = styled.div`
+  height: 70px;
+  background: #aa1818;
+
+  p {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 70px;
+    color: #ffffff;
+    text-align: center;
+  }
+
+  @media (max-width: 420px) {
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+
+    p {
+      font-size: 12px;
+      line-height: 2;
+    }
+  }
 `
